a43b74bb209f2a060a86331269599f18d1f2fa5b
elijahsnoz <elijahsnoz@gmail.com>
2024-08-01 01:43:54 +0100

    gather_data_from_an_API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,63 +6,32 @@ returns information about his/her TODO list progress.
 """
 
 from requests import get
-from sys import argv, exit
-
-
-def fetch_data(url):
-    """Fetch data from a given URL and return JSON response."""
-    try:
-        response = get(url)
-        response.raise_for_status()  # Raise an error for bad responses
-        return response.json()
-    except Exception as e:
-        print(f"Error fetching data: {e}")
-        exit(1)
+from sys import argv
 
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py EMPLOYEE_ID")
-        exit(1)
-
-    try:
-        employee_id = int(argv[1])
-    except ValueError:
-        print("Employee ID must be an integer")
-        exit(1)
-
-    todos_url = 'https://jsonplaceholder.typicode.com/todos'
-    users_url = 'https://jsonplaceholder.typicode.com/users'
-
-    # Fetch todos and users data
-    todos_data = fetch_data(todos_url)
-    users_data = fetch_data(users_url)
-
-    # Find the employee name
-    employee = None
-    for user in users_data:
-        if user.get('id') == employee_id:
-            employee = user.get('name')
-            break
-
-    if not employee:
-        print(f"No employee found with ID {employee_id}")
-        exit(1)
-
-    # Count completed tasks and total tasks
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
     completed = 0
     total = 0
     tasks = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
 
-    for todo in todos_data:
-        if todo.get('userId') == employee_id:
+    for i in data2:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
+
+    for i in data:
+        if i.get('userId') == int(argv[1]):
             total += 1
-            if todo.get('completed'):
+
+            if i.get('completed') is True:
                 completed += 1
-                tasks.append(todo.get('title'))
+                tasks.append(i.get('title'))
 
-    # Print results
-    print(f"Employee {employee} is done with tasks({completed}/{total}):")
-    for task in tasks:
-        print(f"\t {task}")
+    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
+                                                          total))
 
+    for i in tasks:
+        print("\t {}".format(i))
